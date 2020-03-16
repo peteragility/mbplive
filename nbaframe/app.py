5ff837eb90584821fbff644a3cb4c0e716a83856
peterone928 <31908564+peterone928@users.noreply.github.com>
2020-03-16 15:08:24 +0800

    add livetovod job codes

--- nbaframe/app.py
+++ nbaframe/app.py
@@ -4,17 +4,23 @@ from boto3.dynamodb.conditions import Key
 import json
 import datetime
 import decimal
+import uuid
 
 mediaLiveArn = 'arn:aws:medialive:us-west-2:072060221753:channel:3921754'
 captureSecPerFrame = 2
 customLabelModelArn = 'arn:aws:rekognition:us-west-2:072060221753:project/nba-foul/version/nba-foul.2020-03-08T22.09.07/1583676547493'
+foulSecondsBeforeFreeThrow = 20
+secondsBetweenFouls = 40
 
 #dynamodb = boto3.resource('dynamodb', region_name='ap-northeast-1')
 dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
 nbaLiveTable = dynamodb.Table('nbalive')
 nbaFrameTable = dynamodb.Table('nbaframe')
+nbaFoulTable = dynamodb.Table('nbafoul')
 
 rekognition = boto3.client('rekognition', region_name='us-west-2')
+
+mediapackage = boto3.client('mediapackage', region_name='us-west-2')
 
 # Helper class to convert a DynamoDB item to JSON.
 class DecimalEncoder(json.JSONEncoder):
@@ -25,6 +31,48 @@ class DecimalEncoder(json.JSONEncoder):
             else:
                 return int(o)
         return super(DecimalEncoder, self).default(o)
+
+def liveToFoulVod(liveStartTime, freeThrowTime):
+    lastFoulTime = datetime.datetime.strptime('2019-01-01T09:19:19Z','%Y-%m-%dT%H:%M:%SZ')
+    response = nbaFoulTable.query(
+              Limit = 1,
+              ScanIndexForward = False,
+              KeyConditionExpression=Key('arn').eq(mediaLiveArn)
+            )
+    for item in response['Items']:
+        lastFoulTime = datetime.datetime.strptime(item['startTime'],'%Y-%m-%dT%H:%M:%SZ')
+
+    if lastFoulTime >= freeThrowTime - datetime.timedelta(seconds=secondsBetweenFouls):
+        pass
+    else:
+        foulStart = max(freeThrowTime - datetime.timedelta(seconds=foulSecondsBeforeFreeThrow), liveStartTime)
+        jobId = str(uuid.uuid4())
+        foulId = 'nba-foul-' + foulStart.strftime('%Y%m%d%H%M%S')
+
+        print('insert foul capture info into database')
+        response = nbaFoulTable.put_item(
+                    Item={
+                    'arn': mediaLiveArn,
+                    'startTime': foulStart.strftime('%Y-%m-%dT%H:%M:%SZ'),
+                    'endTime': freeThrowTime.strftime('%Y-%m-%dT%H:%M:%SZ'),
+                    'jobId': jobId
+                    }
+                )
+        print (json.dumps(response))
+
+        print('create live to vod harvest job...')
+        response = mediapackage.create_harvest_job(
+            StartTime=foulStart.strftime('%Y-%m-%dT%H:%M:%SZ'),
+            EndTime=freeThrowTime.strftime('%Y-%m-%dT%H:%M:%SZ'),
+            Id=jobId,
+            OriginEndpointId='mbp-nba-channel',
+            S3Destination={
+                'BucketName': 'unicornflix-mbp-output-rdyo3ns5',
+                'ManifestKey': 'fouls/' + foulId +'/index.m3u8',
+                'RoleArn': 'arn:aws:iam::072060221753:role/mediapackage-harvest-role'
+            }
+        )
+        print (json.dumps(response))
 
 
 def lambda_handler(event, context):
@@ -74,7 +122,7 @@ def lambda_handler(event, context):
             if(retData['freethrow']):
                 # Analyze fileKey suffix for timestamp
                 retData['captureSeconds'] = int(retData['fileKey'].split('.')[1]) * captureSecPerFrame
-                fileElapsedSeconds = retData['captureSeconds'] - 3
+                fileElapsedSeconds = retData['captureSeconds'] - 2
 
                 captureTime = liveStartTime + datetime.timedelta(seconds=fileElapsedSeconds)
 
@@ -89,6 +137,10 @@ def lambda_handler(event, context):
                     }
                 )
                 #print(json.dumps(response, indent=4, cls=DecimalEncoder))
+
+                print('check and start live to vod harvest job if required...')
+                liveToFoulVod(liveStartTime, captureTime)
+
             else:
                 print('No free throw found.')
 
